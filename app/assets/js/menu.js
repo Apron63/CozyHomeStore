8dc50c600e25b630530341b7a7e874031b33c46d
Черник Роман Александрович <chernik.romaa@ya.ru>
2025-05-13 00:35:18 +0600

    added imports jquery

--- app/assets/js/menu.js
+++ app/assets/js/menu.js
@@ -1,7 +1,7 @@
+import $ from 'jquery'
+
 $('.menu-btn').on('click',function(e)
 {
-
-
     e.preventDefault;
     $(this).toggleClass('menu-btn_active');
     $('.menu-nav').toggleClass('menu-nav_active');
--- app/assets/js/mfo.js
+++ app/assets/js/mfo.js
@@ -1,79 +1,80 @@
-$(document).ready(function(){
-	$("#menu-humburger-icon").mouseover(function(){
+import $ from 'jquery'
+
+$(document).ready(function () {
+	$("#menu-humburger-icon").mouseover(function () {
 		$("#menu-humburger").show();
 	});
 
-	$("#menu-humburger").mouseleave(function(){
+	$("#menu-humburger").mouseleave(function () {
 		$("#menu-humburger").hide();
 	});
 
 
 
-$('.menu-btn').on('click',function(e)
-{
+	$('.menu-btn').on('click', function (e) {
 
 
-    e.preventDefault;
-    $(this).toggleClass('menu-btn_active');
-    $('.menu-nav').toggleClass('menu-nav_active');
+		e.preventDefault;
+		$(this).toggleClass('menu-btn_active');
+		$('.menu-nav').toggleClass('menu-nav_active');
+	});
+
+
 });
 
 
-});
+function mailing() {
+	$.ajax({
+		url: "php/mailing.php", //url страницы (action_ajax_form.php)
+		type: "POST", //метод отправки
+		dataType: "html", //формат данных
+		data: $("#mailing").serialize(),
+		success: function (msg) {
+			if (msg == "ok") {
+				alert("Спасибо! Вы подписались на рассылку.");
+				$("#mailing")[0].reset();
+			} else {
+				alert(msg);
+			}
 
 
-function mailing(){
- $.ajax({
-            url:     "php/mailing.php", //url страницы (action_ajax_form.php)
-            type:     "POST", //метод отправки
-            dataType: "html", //формат данных
-            data: $("#mailing").serialize(),
-            success:  function(msg){
-                if (msg=="ok") {
-                    alert("Спасибо! Вы подписались на рассылку.");  
-                    $("#mailing")[0].reset(); 
-                } else {
-                    alert(msg);
-                }
+		},
+		error: function (msg) {
+			alert("Ошибка отправки сообщения. Попробуйте еще раз");
+		}
 
-                
-            },
-            error: function (msg) {
-                alert("Ошибка отправки сообщения. Попробуйте еще раз");
-            }
-
-        });
-} 
+	});
+}
 
 
-function message_send(){
-	if($("#politic").prop('checked')){
-		 $.ajax({
-					url:     "php/message_send.php", //url страницы (action_ajax_form.php)
-					type:     "POST", //метод отправки
-					dataType: "html", //формат данных
-					data: $("#contacts-form").serialize(),
-					success:  function(msg){
-						if (msg=="ok") {
-							alert("Спасибо! Ваша заявка принята. В ближайшее время с Вами свяжестся наш представитель.");   
-							$("#contacts-form")[0].reset(); 
-							$("#img-captcha").prop("src","php/captcha.php");
-						} else {
-							$("#img-captcha").prop("src","php/captcha.php");
-							alert(msg);
-						}
-						
-					},
-					error: function (msg) {
-						$("#img-captcha").prop("src","php/captcha.php");
-						alert("Ошибка отправки сообщения. Попробуйте еще раз");
-					}
+function message_send() {
+	if ($("#politic").prop('checked')) {
+		$.ajax({
+			url: "php/message_send.php", //url страницы (action_ajax_form.php)
+			type: "POST", //метод отправки
+			dataType: "html", //формат данных
+			data: $("#contacts-form").serialize(),
+			success: function (msg) {
+				if (msg == "ok") {
+					alert("Спасибо! Ваша заявка принята. В ближайшее время с Вами свяжестся наш представитель.");
+					$("#contacts-form")[0].reset();
+					$("#img-captcha").prop("src", "php/captcha.php");
+				} else {
+					$("#img-captcha").prop("src", "php/captcha.php");
+					alert(msg);
+				}
 
-				});
-	} else{
-		alert( "Согласитесь с обработкой данных ");
+			},
+			error: function (msg) {
+				$("#img-captcha").prop("src", "php/captcha.php");
+				alert("Ошибка отправки сообщения. Попробуйте еще раз");
+			}
+
+		});
+	} else {
+		alert("Согласитесь с обработкой данных ");
 	}
-} 
+}
 
 
 
